6392c5996a8ff4e094284e75274d48f72127964e
Grant Parr <flaneur.lafleur@gmail.com>
2026-04-09 13:17:21 -0500

    Create fan_base_query.sql

---
+++ sql/fan_base_query.sql
@@ -0,0 +1,35 @@
+-- conversion_target_segment.sql
+-- Example portfolio-safe SQL for identifying digitally engaged fans
+-- who have not yet converted to ticket buyers
+
+WITH audience AS (
+    SELECT
+        fan_id,
+        email_address,
+        overall_engagement_score,
+        is_email_subscriber,
+        is_app_user,
+        is_web_user,
+        is_streaming_user,
+        is_ticket_buyer,
+        ticket_last_purchase_date
+    FROM analytics.fan_marketing_dataset
+)
+
+SELECT
+    fan_id,
+    email_address,
+    overall_engagement_score,
+    is_email_subscriber,
+    is_app_user,
+    is_web_user,
+    is_streaming_user
+FROM audience
+WHERE is_ticket_buyer = 0
+  AND overall_engagement_score >= 50
+  AND (
+      is_app_user = 1
+      OR is_web_user = 1
+      OR is_streaming_user = 1
+  )
+ORDER BY overall_engagement_score DESC;
